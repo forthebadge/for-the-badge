a9c555c47d375dacb17b5f6483f6114f7fb9f4dd
jacksongeller <jacksongeller@gmail.com>
2014-09-02 09:29:15 -0400

    gulp init

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require("gulp");
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var svgmin = require('gulp-svgmin');
+var uglify = require('gulp-uglify');
+
+
+
+
