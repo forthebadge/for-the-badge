550caba8f488bed32ceee41727aa5f621f1c2afa
jacksongeller <jacksongeller@gmail.com>
2014-09-02 11:19:35 -0400

    probably not the final dir structure, still thinking about it

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,37 @@ var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
 var svgmin = require('gulp-svgmin');
 var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var rename = require('gulp-rename');
+
+
+var suffix = {suffix: '.min'};
+// scripts
+gulp.task('scripts', function() {
+  gulp.src('./public/scripts/*.js')
+    .pipe(uglify())
+    .pipe(concat('index.js'))
+    .pipe(rename(suffix))
+    .pipe(gulp.dest('./public'));
+});
+
+// css
+gulp.task('css', function() {
+  gulp.src('./public/style.css')
+    .pipe(cssmin())
+    .pipe(rename(suffix))
+    .pipe(gulp.dest('./public'))
+});
+
+// html
+gulp.task('html', function() {
+  gulp.src('./public/index.html')
+    .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+    .pipe(rename(suffix))
+    .pipe(gulp.dest('./public'))
+});
+
+
 
 
 
