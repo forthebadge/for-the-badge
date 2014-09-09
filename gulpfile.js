82db50638ab0c38c3802f0cc282ccc34e094f210
jacksongeller <jacksongeller@gmail.com>
2014-09-09 13:28:20 -0400

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,28 @@ var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
 var svgmin = require('gulp-svgmin');
 var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
-var rename = require('gulp-rename');
 
-
-var suffix = {suffix: '.min'};
 
 gulp.task('scripts', function() {
-  gulp.src('./scripts/*.js')
+  gulp.src('./public/scripts/*.js')
     .pipe(uglify())
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('./scripts'));
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task('css', function() {
-  gulp.src('./style.css')
+  gulp.src('./public/style.css')
     .pipe(cssmin())
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task('svg', function() {
-  gulp.src('./badges/*.svg')
+  gulp.src('./public/badges/*.svg')
     .pipe(svgmin())
-    .pipe(gulp.dest('./badges'));
-  gulp.src('./images/*.svg')
+    .pipe(gulp.dest('./public/badges'));
+  gulp.src('./public/images/*.svg')
     .pipe(svgmin())
-    .pipe(gulp.dest('./images'));
+    .pipe(gulp.dest('./public/images'));
 });
 
 gulp.task('build', ['scripts', 'css', 'svg']);
