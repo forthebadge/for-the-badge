13dff15cbb0aa08da6679e56c28d2624cd6ede91
jacksongeller <jacksongeller@gmail.com>
2014-09-09 14:33:08 -0400

    live

--- server.js
+++ server.js
@@ -16,18 +16,22 @@ app.use(express.Router());
 
 
 app.get('/', function(req, res) {
+  
   fs.readdir('./public/badges', function(err, files) {
     if (err) res.send(500);
+    
     var max = files.length;
     var holder = []
+    
     for (var i = 0; i < max; i += 1) {
       holder.push({
         badgeName: files[i].split('.svg')[0].replace(/-/g,' '),
-        badgeSrc: 'badges/' + files[i],
-        badgeMrkdn: '[![forthebadge](http://forthebadge.com/badges/' + files[i] + ')](http://forthebadge.com)',
-        badgeUrl: 'http://forthebadge.com/badges/' + files[i] 
+        badgeSrc: ['badges/', files[i]].join(''),
+        badgeMrkdn: ['[![forthebadge](http://forthebadge.com/badges/', files[i], ')](http://forthebadge.com)'].join(''),
+        badgeUrl: ['http://forthebadge.com/badges/', files[i] ].join('')
       });
     }
+    
     res.render('index', {badges: holder});  
   });
 });
