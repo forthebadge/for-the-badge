0d54c5f2e6b9af6f58319dd3d9d26e39b1240283
jacksongeller <jacksongeller@gmail.com>
2014-09-09 13:20:05 -0400

    restructured

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+var public = __dirname + '/public';
+
+app.use(express.static(public));
 app.set('view engine', 'ejs');
+app.set('views', public);
 app.use(express.Router());
 
 app.get('/', function(req, res) {
-  fs.readdir('./badges', function(err, files) {
+  fs.readdir('./public/badges', function(err, files) {
     if (err) res.send(500);
     var max = files.length;
     var holder = []
     for (var i = 0; i < max; i += 1) {
       holder.push({
-        badgeName: files[i].split('.svg')[0].replace('-',' '),
+        badgeName: files[i].split('.svg')[0].replace(/-/g,' '),
         badgeSrc: 'badges/' + files[i],
         badgeMrkdn: '[![forthebadge](http://forthebadge.com/badges/' + files[i] + ')](http://forthebadge.com)',
         badgeUrl: 'http://forthebadge.com/badges/' + files[i] 
