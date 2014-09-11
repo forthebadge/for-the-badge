5755c33f5078f0158deeb9113d67cd5ce11714b7
jacksongeller <jacksongeller@gmail.com>
2014-09-11 14:45:53 -0400

    may be the most hacky thing I've ever done

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var helpers = require('./helpers');
 var compress = require('compression');
+var fs = require('fs');
+var templater = require('svg-templater');
 
 
 var public = __dirname + '/public';
@@ -21,10 +23,27 @@ app.get('/', function(req, res) {
   });
 });
 
+app.get('/generate/:badgeText/:color1?/:color2?', function(req, res) {
+  res.type('image/svg+xml;charset=utf-8');
 
-app.all('/*', function(req, res) {
-  res.redirect('/');
+
+  if (!req.params.badgeText || req.params.badgeText.split('-').length <= 1) {
+    res.send('Malformed badge, need a dash between text');
+  }
+  var badgeText = req.params.badgeText.split('-');
+  var color1 = req.params.color1 || '#EA4460';
+  var color2 = req.params.color2 || '#C13A3A';
+
+  templater.compileFromFile('./generator/clientTemplate.svg', {text:badgeText, color1: color1, color2: color2}, function(err, data) {
+    if (err) res.send(err);
+    if (data) {
+      res.send(data)
+    }
+  });  
 });
+
+
+
 
 app.listen(3000);
 
