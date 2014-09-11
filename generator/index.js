b334b952686edd8afe0fef5ef4cc549d148ebc9a
jacksongeller <jacksongeller@gmail.com>
2014-09-11 16:59:52 -0400

    think we good

--- generator/index.js
+++ generator/index.js
@@ -5,7 +5,7 @@ var templater = require('svg-templater');
 http.createServer(function(req, res) {
   var url = req.url;
   url = url.substr(1, req.url.length);
-    
+
   res.writeHead(200, {
     'Content-Type': 'image/svg+xml'
   });
@@ -14,8 +14,10 @@ http.createServer(function(req, res) {
 
 
   if ( isSvg.test(url) ) {
-    var data = url.replace('.svg','').split('-');
-    
+    var data = url.replace(/%20/g,' ').replace('.svg','').split('-');
+    data[2] = data[2] || '#86C8D6';
+    data[3] = data[3] || '#095382';
+
     templater.compileFromFile('./generator/clientTemplate.svg', {data:data}, function(err, data) {
       if (err) {
         res.write(err);
