1e830be2de3a1bee58388c9047476e43f5fade7d
jacksongeller <jacksongeller@gmail.com>
2014-09-11 16:39:17 -0400

    cleanup

--- generator/index.js
+++ generator/index.js
@@ -5,9 +5,7 @@ var templater = require('svg-templater');
 http.createServer(function(req, res) {
   var url = req.url;
   url = url.substr(1, req.url.length);
-
-  var filepath = __dirname +'/clientTemplate.svg';
-  
+    
   res.writeHead(200, {
     'Content-Type': 'image/svg+xml'
   });
@@ -16,10 +14,9 @@ http.createServer(function(req, res) {
 
 
   if ( isSvg.test(url) ) {
-    var badgeText = url.split('-');
-    var colors = url.split(':')[1].split('-');
+    var data = url.replace('.svg','').split('-');
     
-    templater.compileFromFile('./generator/clientTemplate.svg', {text:badgeText, colors: colors}, function(err, data) {
+    templater.compileFromFile('./generator/clientTemplate.svg', {data:data}, function(err, data) {
       if (err) {
         res.write(err);
         res.end();
